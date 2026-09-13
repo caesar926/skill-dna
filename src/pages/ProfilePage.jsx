c7729e751878b9d9692748458ce92bbc8fbe9932
caesar926 <caesarexorvus15@gmail.com>
2026-09-13 13:54:38 +0100

    update profilePage

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -2,6 +2,8 @@ import { useParams } from 'react-router-dom'
 import { usePersonalProfile } from '../hooks/usePersonalProfile'
 import { Skeleton } from '../components/Skeleton'
 import DevProfile from '../components/DevProfile'
+import Heatmap from '../components/Heatmap'
+import RepoCard from '../components/RepoCard'
 
 export function ProfilePage({apiBase}) {
   const { username } = useParams()
@@ -15,13 +17,31 @@ export function ProfilePage({apiBase}) {
 ) : status === "error" ? (
   <p>{errorMessage}</p>
 ) : (
- <DevProfile profile={{
+  <>
+     <DevProfile profile={{
   avatar_url: profile.data?.avatarUrl,
   login: profile.github_username,
   bio: profile.data?.bio,
   public_repos: profile.data?.repositories?.totalCount ?? 0,
   followers: profile.data?.followers?.totalCount ?? 0,
 }} />
+  {(profile.data?.pinnedItems?.nodes ?? []).map((repo) => (
+  <RepoCard
+    key={repo.id}
+    repo={{
+      html_url: repo.url,
+      name: repo.name,
+      description: repo.description,
+      language: repo.primaryLanguage?.name,
+      stargazers_count: repo.stargazerCount,
+      forks_count: repo.forkCount,
+    }}
+  />
+))}
+
+  <Heatmap contributionData={profile.data?.contributionsCollection?.contributionCalendar} />
+  </>
+
 )
   )
  
