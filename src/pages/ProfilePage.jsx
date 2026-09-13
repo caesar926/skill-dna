abe5fe9c8e84aa954125a56d6232ad23a581f937
caesar926 <caesarexorvus15@gmail.com>
2026-09-13 15:24:04 +0100

    restructure profilePage

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -5,46 +5,54 @@ import DevProfile from '../components/DevProfile'
 import Heatmap from '../components/Heatmap'
 import RepoCard from '../components/RepoCard'
 
-export function ProfilePage({apiBase}) {
+export function ProfilePage({ apiBase}) {
   const { username } = useParams()
   const { status, errorMessage, profile } = usePersonalProfile(username, apiBase)
-  
-  return(
-  status === "loading" ? (
-  <Skeleton />
-) : status === "notFound" ? (
-  <p>This developer hasn't claimed their profile yet</p>
-) : status === "error" ? (
-  <p>{errorMessage}</p>
-) : (
-  <>
-     <DevProfile profile={{
-  avatar_url: profile.data?.avatarUrl,
-  login: profile.github_username,
-  bio: profile.data?.bio,
-  public_repos: profile.data?.repositories?.totalCount ?? 0,
-  followers: profile.data?.followers?.totalCount ?? 0,
-}} />
-  {(profile.data?.pinnedItems?.nodes ?? []).map((repo) => (
-  <RepoCard
-    key={repo.id}
-    repo={{
-      html_url: repo.url,
-      name: repo.name,
-      description: repo.description,
-      language: repo.primaryLanguage?.name,
-      stargazers_count: repo.stargazerCount,
-      forks_count: repo.forkCount,
-    }}
-  />
-))}
 
-  <Heatmap contributionData={profile.data?.contributionsCollection?.contributionCalendar} />
-  </>
+  return (
+    status === "loading" ? (
+      <Skeleton />
+    ) : status === "notFound" ? (
+      <p>This developer hasn't claimed their profile yet</p>
+    ) : status === "error" ? (
+      <p>{errorMessage}</p>
+    ) : (
+      <>
+          <main className='dashboard-container'>
+        <aside className='left-sidebar'>
+          <DevProfile profile={{
+            avatar_url: profile.data?.avatarUrl,
+            login: profile.github_username,
+            bio: profile.data?.bio,
+            public_repos: profile.data?.repositories?.totalCount ?? 0,
+            followers: profile.data?.followers?.totalCount ?? 0,
+          }} />
+        </aside>
 
-)
+        <section className='main-content'>
+          {( profile.data?.pinnedItems?.nodes ?? []).map((repo) => (
+            <RepoCard
+              key={repo.id}
+              repo={{
+                html_url: repo.url,
+                name: repo.name,
+                description: repo.description,
+                language: repo.primaryLanguage?.name,
+                stargazers_count: repo.stargazerCount,
+                forks_count: repo.forkCount,
+              }}
+            />
+          ))}
+        </section>
+      </main>
+
+           <Heatmap contributionData={profile.data?.contributionsCollection?.contributionCalendar} />
+
+      </>
+    
+    )
   )
- 
+
 }
 
 export default ProfilePage
