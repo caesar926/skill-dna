61f99f943f5286ba09213702dc1663785854870d
caesar926 <caesarexorvus15@gmail.com>
2026-09-19 19:02:58 +0100

    fix app

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
 
   const isHomePage = location.pathname === '/';
 
-  const showSearchBar = !isHomePage || Boolean(searchedUser) || profileData;
+  const showSearchBar = !isHomePage || Boolean(searchedUser) || !profileData;
 
   const {
     isLoggedIn,
